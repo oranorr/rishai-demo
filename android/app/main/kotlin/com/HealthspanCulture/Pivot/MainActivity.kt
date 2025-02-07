12fb547a304454e3a2c4fa369e11e24cdba605b1
iaroslav <oliverkarlin0@gmail.com>
2025-02-07 16:37:24 +0700

    android builds

---
+++ android/app/main/kotlin/com/HealthspanCulture/Pivot/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.HealthspanCulture.Pivot
+
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity(){
+}
---
+++ android/app/src/main/kotlin/com/HealthspanCulture/rishai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.HealthspanCulture.Pivot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
